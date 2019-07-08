773f5c7204de3c6df49ace4b7a553c20c9366fdb
zhuhai <hpuzhuhai@gmail.com>
2019-07-08 23:20:06 +0800

    数组队列和循环队列性能比较

---
+++ src/main/java/com/zhuhai/TestQueue.java
@@ -0,0 +1,41 @@
+package com.zhuhai;
+
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2019/7/8
+ * Time: 23:04
+ *
+ * @author: hai
+ */
+public class TestQueue {
+
+    public static void main(String[] args) {
+        int count = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = test(arrayQueue, count);
+        System.out.println("ArrayQueue time: " + time1);
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = test(loopQueue, count);
+        System.out.println("LoopQueue time: " + time2);
+
+
+    }
+
+
+    private static double test(Queue<Integer> queue, int count) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < count; i++) {
+            queue.dequeue();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+}
