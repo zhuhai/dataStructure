1cdb33a869248ab0d92a15d2089ad4939f75b3a2
zhuhai <hpuzhuhai@gmail.com>
2019-07-11 00:05:59 +0800

    LeetCode 203题

---
+++ leeCode/src/main/java/com/zhuhai/ListNode.java
@@ -0,0 +1,16 @@
+package com.zhuhai;
+
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2019/7/10
+ * Time: 23:45
+ *
+ * @author: hai
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ leeCode/src/main/java/com/zhuhai/Solution.java
@@ -0,0 +1,37 @@
+package com.zhuhai;
+
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2019/7/10
+ * Time: 23:47
+ *
+ * @author: hai
+ */
+public class Solution {
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.val == val) {
+            /*ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;*/
+
+            head = head.next;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                /*ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;*/
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+}
