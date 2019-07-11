f47fbeea3a52c548ead59e96f0a7a37e4fe7dadc
ChinaSea <hpuzhuhai@163.com>
2019-07-11 15:05:30 +0800

    leetCode 203题测试

--- src/main/java/com/zhuhai/leetCode/ListNode.java
+++ src/main/java/com/zhuhai/leetCode/ListNode.java
@@ -13,4 +13,29 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("arr cannot be empty");
+        }
+        this.val = arr[0];
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode cur = this;
+        while (cur != null) {
+            sb.append(cur.val + "—>");
+            cur = cur.next;
+        }
+        sb.append("NULL");
+        return sb.toString();
+    }
 }
--- src/main/java/com/zhuhai/leetCode/Solution2.java
+++ src/main/java/com/zhuhai/leetCode/Solution2.java
@@ -1,46 +1,48 @@
 package com.zhuhai.leetCode;
-
 /**
- * Created with IntelliJ IDEA
- * Date: 2019/7/10
- * Time: 23:47
+ * Created with IntelliJ IDEA.
+ * Date: 2019/7/11
+ * Time: 10:12
  *
- * @author: hai
+ * @author: zhuhai
  */
-public class Solution {
+public class Solution2 {
 
+
+    /**
+     * 使用虚拟头节点删除链表中指定的元素
+     * @param head
+     * @param val
+     * @return
+     */
     public ListNode removeElements(ListNode head, int val) {
 
-        while (head != null && head.val == val) {
-            /*ListNode delNode = head;
-            head = head.next;
-            delNode.next = null;*/
-            head = head.next;
-        }
-        if (head == null) {
-            return null;
-        }
-        ListNode prev = head;
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode prev = dummyNode;
         while (prev.next != null) {
             if (prev.next.val == val) {
-                /*ListNode delNode = prev.next;
+               /* ListNode delNode = prev.next;
                 prev.next = delNode.next;
                 delNode.next = null;*/
                 prev.next = prev.next.next;
             } else {
                 prev = prev.next;
             }
+
         }
-        return head;
+        return dummyNode.next;
+
     }
 
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,3,6};
         ListNode listNode = new ListNode(arr);
         System.out.println(listNode);
-        Solution solution = new Solution();
-        solution.removeElements(listNode, 6);
+        Solution2 solution2 = new Solution2();
+        solution2.removeElements(listNode, 6);
         System.out.println(listNode);
     }
+
 
 }
